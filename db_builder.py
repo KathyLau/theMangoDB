fc0c0eddc4137afe0f55f65556d309522aa4fc33
Kathy Lau <imobilep501@gmail.com>
2017-02-04 23:26:17 -0500

    fix

--- db_builder.py
+++ db_builder.py
@@ -4,14 +4,12 @@ server = MongoClient('localhost')
 db = server.theMangoDB
 
 if db.students.count() == 0:
-    p=[r for r in DictReader(open("peeps.csv"))]
-    c=[r for r in DictReader(open("courses.csv"))]
-    mainList = [] #to hold list of updated dictionaries
+    p = DictReader(open("peeps.csv"))
     for peep in p:
-        courseD = {}
+        courses = {}
+        c = DictReader(open("courses.csv"))
         for course in c:
             if course['id'] == peep['id']:
-                courseD[course['code']] = course['mark'] #store as {'course1':mark, etc}
-        peep['courses'] = courseD #add courses to student dictionary
-        mainList.append(peep)
-    db.students.insert_many(mainList)
+                courses[course['code']] = course['mark'] #store as {'course1':mark, etc}
+        peep['courses'] = courses #add courses to student dictionary
+        db.students.insert_one(peep)
