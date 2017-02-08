b9212d601b8c65839e79d5cd8907f7135c67dade
Kathy Lau <imobilep501@gmail.com>
2017-02-07 23:19:43 -0500

    new format for db builder

--- db_builder.py
+++ db_builder.py
@@ -1,4 +1,4 @@
-#Team 'TheMangoDB'
+#Team 'TheMangoDB
 #Kathy Lau, Felix Rieg-Baumhauer
 
 from pymongo import MongoClient
@@ -6,14 +6,16 @@ from csv import DictReader       #facilitates CSV I/O
 server = MongoClient('149.89.150.100')
 db = server.theMangoDB
 
-if db.students.count() == 0:
-    p = DictReader(open("peeps.csv"))
-    for peep in p:
-        courses = {}
-        c = DictReader(open("courses.csv"))
-        for course in c:
-            if course['id'] == peep['id']:
-                courses[course['code']] = course['mark'] #store as {'course1':mark, etc}
-        peep['courses'] = courses #add courses to student dictionary
-        db.students.insert_one(peep)
+db.students.drop()
+p = DictReader(open("peeps.csv"))
+c = [course for course in DictReader(open("courses.csv"))]
 
+for peep in p:
+    mList = []
+    for course in c:
+        if course['id'] == peep['id']:
+           mList.append({'code': course['code'], 'mark': int(course['mark'])})
+    peep['courses'] = mList #add courses to student dictionary
+    peep['age'] = int(peep['age']) #convert all to int
+    peep['id'] = int(peep['id'])
+    db.students.insert_one(peep)
--- teacher_builder.py
+++ teacher_builder.py
@@ -3,14 +3,17 @@
 
 from pymongo import MongoClient
 from csv import DictReader       #facilitates CSV I/O
-server = MongoClient('149.89.150.100')
+server = MongoClient()
 db = server.theMangoDB
 
 db.teachers.drop()
-
 t = DictReader(open("teachers.csv"))
-for teacher in t:
-	students = db.students.find({'courses'.name:teacher['code']})
-	teacher['students'] = students	
-	db.insert_one({teacher})
 
+for teacher in t:
+	students = db.students.find({'courses.code':teacher['code']})
+	s=[]
+	for stud in students:
+		s.append(stud['_id'])
+	teacher['peeps'] = s
+	teacher['period'] = int(teacher['period'])
+	db.teachers.insert_one(teacher)
